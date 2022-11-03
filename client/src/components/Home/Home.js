5f56dbdc88cae20c780c4afb16c79ccc7835fe7f
XuperBryan <bryanxu@ucsb.edu>
2022-11-02 17:14:52 -0700

    bx - added working version of auth frontend

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import useStyles from './styles';
+
+const Home = () => {
+    const classes = useStyles();
+    
+    return (
+        <h1>Welcome to the homepage!</h1>
+    );
+};
+
+export default Home;
