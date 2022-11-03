6d0deffaf87b3e3b063fd3d48b0fc5d01bc4d7be
Henry Zhang <henry.zhang90@gmail.com>
2022-11-02 19:19:21 -0700

    xz - added navigation bar to all pages, but no search bar yet

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import { Container } from '@material-ui/core';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
+import Navbar from './components/common/Navbar/Navbar';
 
 const App = () => (
     <BrowserRouter>
         <Container maxWidth="lg">
+            <Navbar />
             <Routes>
                 <Route path="/" exact element={<Home />}/>
                 <Route path="/auth" exact element={<Auth />}/>
