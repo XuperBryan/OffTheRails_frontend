9cb656e306928e2583fb3ff94347e8bd93f17767
XuperBryan <bryanxu@ucsb.edu>
2022-11-04 00:29:30 -0700

    bx - remove unused comma

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React, { useState, } from 'react';
+import React, { useState } from 'react';
 import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import useStyles from './styles';
