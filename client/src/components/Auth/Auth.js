9f82ec705c36f02559a8983327fa01427c057257
Vivian Ross <vivianross06@users.noreply.github.com>
2022-11-03 22:54:54 -0700

    removed unused imports

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState, } from 'react';
 import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import useStyles from './styles';
