c0342460a7f474bf4b3b464ce255e5fd33731f13
Sydney Lim <sydneylim@ucsb.edu>
2022-11-03 21:01:53 -0700

    sl - trying to add item cards to home

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import useStyles from './styles';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { Grid } from '@material-ui/core';
+
 
 const Home = () => {
+    const items = useSelector((state) => state.items);
     const classes = useStyles();
     
     return (
-        <h1>Welcome to the homepage!</h1>
+        {items}
+        // <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        //     {items.map((item) => (
+        //         <Grid key={item._id} item xs={12} sm={6} md={6}>
+        //             <Card sx={{ maxWidth: 345 }}>
+        //                 <CardActionArea>
+        //                     <CardMedia
+        //                         component="img"
+        //                         height="140"
+        //                         image={item.image_link}
+        //                         alt={item.name}
+        //                     />
+        //                     <CardContent>
+        //                         <Typography gutterBottom variant="h5" component="div">
+        //                         {item.name}
+        //                         </Typography>
+        //                         <Typography variant="body2" color="text.secondary">
+        //                         {item.price}
+        //                         </Typography>
+        //                     </CardContent>
+        //                 </CardActionArea>
+        //             </Card>                
+        //         </Grid>
+        //     ))}
+        // </Grid>
     );
 };
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,12 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
-import authReducer from './reducers/auth.js';
+import { reducers } from './reducers';
 
 import App from './App';
 import './index.css';
 
-const store = createStore(authReducer, compose(applyMiddleware(thunk)));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
     <Provider store={store}>
