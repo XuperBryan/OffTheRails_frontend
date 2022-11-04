b2170a581f204dab32faf0ccdca16fde7c05521d
XuperBryan <bryanxu@ucsb.edu>
2022-11-03 21:48:51 -0700

    bx - added dispatch to make api calls to home

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:3000' });
+const API = axios.create({ baseURL: 'http://localhost:3000' }); // replace with deployment link
 // const API = axios.create({ baseURL: 'http://localhost:3000' });
 
 API.interceptors.request.use((req) => {
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import useStyles from './styles';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -7,38 +7,44 @@ import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { Grid } from '@material-ui/core';
-
+import { getItems } from '../../actions/items';
+import { useDispatch } from 'react-redux';
 
 const Home = () => {
-    const items = useSelector((state) => state.items);
     const classes = useStyles();
+    const dispatch = useDispatch();
     
+    useEffect(() => {
+        dispatch(getItems());
+    }, []);
+
+    const items = useSelector((state) => state.items);
+
     return (
-        {items}
-        // <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-        //     {items.map((item) => (
-        //         <Grid key={item._id} item xs={12} sm={6} md={6}>
-        //             <Card sx={{ maxWidth: 345 }}>
-        //                 <CardActionArea>
-        //                     <CardMedia
-        //                         component="img"
-        //                         height="140"
-        //                         image={item.image_link}
-        //                         alt={item.name}
-        //                     />
-        //                     <CardContent>
-        //                         <Typography gutterBottom variant="h5" component="div">
-        //                         {item.name}
-        //                         </Typography>
-        //                         <Typography variant="body2" color="text.secondary">
-        //                         {item.price}
-        //                         </Typography>
-        //                     </CardContent>
-        //                 </CardActionArea>
-        //             </Card>                
-        //         </Grid>
-        //     ))}
-        // </Grid>
+        <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+            {items.map((item) => (
+                <Grid key={item._id} item xs={12} sm={6} md={6}>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image={item.image_link}
+                                alt={item.name}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                {item.name}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                ${item.price}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>                
+                </Grid>
+            ))}
+        </Grid>
     );
 };
 
